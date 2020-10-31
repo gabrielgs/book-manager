ca32e740078902dc0f67a31db0ce94f89c928146
Gabriel Garcia <gabrielgsdev@gmail.com>
2020-10-30 22:25:53 -0600

    Add Books Model and migration

---
+++ database/migrations/2020_10_31_025226_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements();
+            $table->string('name', 100);
+            $table->string('author', 100);
+            $table->string('status', 50);
+            $table->string('user_borrow', 100);
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
