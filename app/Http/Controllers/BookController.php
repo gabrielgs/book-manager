1a570619d83dc68f2d84c4209e8adc2557e5425b
Gabriel Garcia <gabrielgsdev@gmail.com>
2020-10-31 23:02:26 -0600

    Add resources to consume api

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class BooksController extends Controller
+class BookController extends Controller
 {
     /**
      * Display a listing of the resource.
