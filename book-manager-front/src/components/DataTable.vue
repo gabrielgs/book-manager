ca9c7a2db787703db74a1b93f8151126f95a8488
Gabriel Garcia <gabrielgsdev@gmail.com>
2020-11-02 01:29:33 -0600

    Fetch data and send to DataTable

--- book-manager-front/src/components/DataTable.vue
+++ book-manager-front/src/components/DataTable.vue
@@ -3,17 +3,26 @@
     <table class="data-table bg-white border-collapse w-full">
       <thead class="text-center bg-gray-300">
         <tr>
+          <th>ID</th>
           <th>Name</th>
           <th>Author</th>
           <th>Status</th>
           <th>Borrow To</th>
           <th>Publication Date</th>
+          <th>Category</th>
           <th>Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="i in 5" :key="i" class=" border-b border-gray-300">
-          <td v-for="i in 5" :key="i" class=" text-center">Sherlock</td>
+        <tr
+          v-for="book in books"
+          :key="book.id"
+          class=" border-b border-gray-300"
+        >
+          <td v-for="(value, key) in book" :key="key" class=" text-center">
+            <span v-if="key == 'category'">{{ value.name }}</span>
+            <span v-else>{{ value }}</span>
+          </td>
           <td class=" text-center">
             <button
               class="bg-blue-500 rounded text-white  text-sm px-3 py-1 mr-3"
@@ -32,14 +41,19 @@
 
 <script>
 export default {
-  name: "DataTable"
+  name: "DataTable",
+  props: {
+    books: {
+      type: Array
+    }
+  }
 };
 </script>
 
 <style scoped>
 th,
 td {
-  padding: 16px;
+  padding: 10px;
   vertical-align: middle;
 }
 </style>
