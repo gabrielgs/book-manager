21c33bb4441cadc15b6b7ff684e37b7e4b37afa0
Gabriel Garcia <gabrielgsdev@gmail.com>
2020-11-02 21:36:38 -0600

    Add more books

--- book-manager-api/database/seeders/BookSeeder.php
+++ book-manager-api/database/seeders/BookSeeder.php
@@ -60,7 +60,134 @@ class BookSeeder extends Seeder
             'user_borrow' => 'Joaquín Ruiz',
             'publication_date' => '2020-09-29',
             'category_id' => 9,
+        ],
+
+        [
+            'name' => 'Death of a Salesman',
+            'author' => 'Arthur Miller',
+            'status' => 'Borrowed',
+            'user_borrow' => 'William Shneider',
+            'publication_date' => '1949-09-29',
+            'category_id' => 3,
+        ],
+
+        [
+            'name' => 'Hamlet',
+            'author' => 'William Shakespeare',
+            'status' => 'Available',
+            'user_borrow' => '',
+            'publication_date' => '1609-09-29',
+            'category_id' => 3,
+        ],
+
+        [
+            'name' => 'The Thursday Murder Club',
+            'author' => 'Richard Osman',
+            'status' => 'Available',
+            'user_borrow' => '',
+            'publication_date' => '2020-09-03',
+            'category_id' => 7,
+        ],
+
+        [
+            'name' => '19th Christmas',
+            'author' => 'James Patterson',
+            'status' => 'Borrowed',
+            'user_borrow' => 'Richard Johnson',
+            'publication_date' => '2010-10-07',
+            'category_id' => 7,
+        ],
+
+        [
+            'name' => 'Ready Player Ones',
+            'author' => 'Ernest Cline',
+            'status' => 'Borrowed',
+            'user_borrow' => 'Sofia Rivero',
+            'publication_date' => '2011-08-16',
+            'category_id' => 1,
+        ],
+
+        [
+            'name' => 'Skyhunter',
+            'author' => 'Marie Lu',
+            'status' => 'Borrowed',
+            'user_borrow' => 'Joaquín Ruiz',
+            'publication_date' => '2020-09-29',
+            'category_id' => 5,
+        ],
+
+        [
+            'name' => 'War Lord',
+            'author' => 'Bernard Cornwell',
+            'status' => 'Borrowed',
+            'user_borrow' => 'Alejandro Rivero',
+            'publication_date' => '2020-10-15',
+            'category_id' => 1,
+        ],
+
+        [
+            'name' => 'Moby Dick',
+            'author' => 'Herman Melville',
+            'status' => 'Borrowed',
+            'user_borrow' => 'Gerardo Ruiz',
+            'publication_date' => '1851-04-27',
+            'category_id' => 1,
+        ],
+
+        [
+            'name' => 'Outlander',
+            'author' => 'Diana Gabaldon',
+            'status' => 'Available',
+            'user_borrow' => '',
+            'publication_date' => '1991-07-01',
+            'category_id' => 5,
+        ],
+
+        [
+            'name' => 'Snow Flower and the Secret Fan',
+            'author' => 'Lisa See',
+            'status' => 'Not Available',
+            'user_borrow' => '',
+            'publication_date' => '2005-07-28',
+            'category_id' => 5,
+        ],
+
+        [
+            'name' => 'Me Before You',
+            'author' => 'Jojo Moyes',
+            'status' => 'Borrowed',
+            'user_borrow' => 'Juan Perez',
+            'publication_date' => '2012-01-05',
+            'category_id' => 8,
+        ],
+
+        [
+            'name' => 'It Ends With Us',
+            'author' => 'Colleen Hoover',
+            'status' => 'Available',
+            'user_borrow' => '',
+            'publication_date' => '2016-08-02',
+            'category_id' => 8,
+        ],
+
+        [
+            'name' => 'The Stand',
+            'author' => 'Stephen King',
+            'status' => 'Not Available',
+            'user_borrow' => '',
+            'publication_date' => '1978-10-03',
+            'category_id' => 9,
+        ],
+
+        [
+            'name' => 'Dune',
+            'author' => 'Frank Herbert',
+            'status' => 'Available',
+            'user_borrow' => '',
+            'publication_date' => '1965-08-01',
+            'category_id' => 9,
         ]
+
     ];
 
     /**
